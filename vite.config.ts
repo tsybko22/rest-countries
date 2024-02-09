8246520b1e19ec69f5225b485c9d84fa4b0927ca
Ivan Tsybko <tsybko22@gmail.com>
2024-02-09 13:48:26 +0200

    redesign aliases

--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,13 @@ import { defineConfig } from 'vite';
 
 import react from '@vitejs/plugin-react';
 
-const aliases = ['', 'components', 'pages', 'helpers', 'images'];
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    alias: aliases.map((alias) => ({
-      find: `@${alias}`,
-      replacement: resolve(__dirname, `src/${alias}`),
-    })),
+    alias: {
+      '@': resolve(__dirname, './src'),
+      '@images': resolve(__dirname, './src/assets/images'),
+    },
   },
 });
