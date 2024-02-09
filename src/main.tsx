c0f59354c0e6986f30e0bf9734342d872ed7c150
Ivan Tsybko <tsybko22@gmail.com>
2024-02-09 14:55:00 +0200

    remove scss styles import

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 
-import '@/styles/index.scss';
-
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
