63cbb11bf37cb8dafe8cc8e3bafe26ce9bc664de
Ivan Tsybko <tsybko22@gmail.com>
2024-02-10 16:33:50 +0200

    add MainPage stub component

---
+++ src/pages/MainPage/MainPage.tsx
@@ -0,0 +1,12 @@
+import Search from '@/components/Search';
+
+const MainPage = () => {
+  return (
+    <>
+      <p>Found 259 countries</p>
+      <Search />
+    </>
+  );
+};
+
+export default MainPage;
