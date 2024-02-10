c515b0e23ee4fc0a7e1a7a96dd3e8faf66a11247
Ivan Tsybko <tsybko22@gmail.com>
2024-02-10 15:31:39 +0200

    add & initialize react-router-dom package

---
+++ src/Router.tsx
@@ -0,0 +1,30 @@
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
+import CountryDetailsPage from '@/pages/CountryDetailsPage';
+import ErrorPage from '@/pages/ErrorPage';
+import MainPage from '@/pages/MainPage';
+import BaseLayout from './layouts/BaseLayout';
+
+const Router = () => {
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <BaseLayout />,
+      children: [
+        {
+          index: true,
+          element: <MainPage />,
+        },
+        {
+          path: 'country/:countryName',
+          element: <CountryDetailsPage />,
+        },
+        { path: '*', element: <ErrorPage /> },
+      ],
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
+};
+
+export default Router;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
+import Router from './Router.tsx';
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Router />
   </React.StrictMode>
 );
