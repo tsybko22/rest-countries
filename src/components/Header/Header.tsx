94e119a65bd327850a898cf88b62f999a390d4a4
Ivan Tsybko <tsybko22@gmail.com>
2024-02-10 16:57:54 +0200

    delete logo image

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,20 +2,11 @@ import { Link } from 'react-router-dom';
 
 import styles from './Header.module.scss';
 
-import logoSvg from '@images/logo.svg';
-
 const Header = () => {
   return (
     <header className={styles.header}>
-      <Link
-        to="/"
-        className={styles.logo}
-      >
-        <img
-          src={logoSvg}
-          alt="Logo"
-        />
-        REST Countries
+      <Link to="/">
+        <span className={styles.logo}>REST Countries</span>
       </Link>
     </header>
   );
