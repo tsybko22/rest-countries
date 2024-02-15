f00b1c277a0abe87b8681ca79c9e58b9affc2810
Ivan Tsybko <tsybko22@gmail.com>
2024-02-15 17:42:43 +0200

    add CountriesTable component

---
+++ src/components/CountriesTable/CountriesTable.tsx
@@ -0,0 +1,50 @@
+import { Link } from 'react-router-dom';
+
+import { type Country } from '@/api/index.dto';
+
+import styles from './CountriesTable.module.scss';
+
+interface CountriesTableProps {
+  countries: Country[];
+}
+
+const CountriesTable = ({ countries }: CountriesTableProps) => {
+  return (
+    <table className={styles.table}>
+      {
+        <thead>
+          <tr>
+            <th className={styles.tableHeading}>Flag</th>
+            <th className={styles.tableHeading}>Name</th>
+            <th className={styles.tableHeading}>Population</th>
+            <th className={styles.tableHeading}>Area (km²)</th>
+            <th className={styles.tableHeading}>Region</th>
+          </tr>
+        </thead>
+      }
+      <tbody>
+        {countries.map((country) => (
+          <tr key={country.cca2}>
+            <td className={styles.tableData}>
+              <img
+                className={styles.tableImg}
+                src={country.flags.svg}
+                alt={country.flags.alt}
+              />
+            </td>
+            <td className={styles.tableData}>
+              <Link to={`/country/${country.name.common.toLowerCase()}`}>
+                {country.name.common}
+              </Link>
+            </td>
+            <td className={styles.tableData}>{country.population.toLocaleString()}</td>
+            <td className={styles.tableData}>{country.area.toLocaleString()}</td>
+            <td className={styles.tableData}>{country.region}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+export default CountriesTable;
