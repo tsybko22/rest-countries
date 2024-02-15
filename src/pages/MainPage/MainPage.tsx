5b59c94fb7a441c7d0989d9cd615b7d8aa67a3c9
Ivan Tsybko <tsybko22@gmail.com>
2024-02-15 17:44:17 +0200

    add CountriesTable component to MainPage

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,10 +1,40 @@
-import Search from '@/components/Search';
+import { Navigate } from 'react-router-dom';
 
-const MainPage = () => {
+import { type Country } from '@/api/index.dto';
+
+import Search from '@/components/Search';
+import CountriesSkeleton from '@/components/ui/CountriesSkeleton';
+
+import CountriesTable from '@/components/CountriesTable';
+import styles from './MainPage.module.scss';
+
+interface MainPageProps {
+  countries: Country[] | null | undefined;
+  isLoading: boolean;
+  error: Error | null;
+}
+
+const MainPage = ({ countries, isLoading, error }: MainPageProps) => {
+  if (error) {
+    return (
+      <Navigate
+        to="/error-page"
+        replace={true}
+      />
+    );
+  }
+
   return (
     <>
-      <p>Found 259 countries</p>
-      <Search />
+      <div className={styles.mainPageHeader}>
+        <p className={styles.mainPageTitle}>Found {countries?.length || 0} countries</p>
+        <Search />
+      </div>
+      {countries && !isLoading ? (
+        <CountriesTable countries={countries} />
+      ) : (
+        <CountriesSkeleton count={10} />
+      )}
     </>
   );
 };
