6bfc5600364e58706144e3b29718389b88a16897
Ivan Tsybko <tsybko22@gmail.com>
2024-02-15 23:14:22 +0200

    refactor getCountryByCode function

--- src/api/countriesApi.ts
+++ src/api/countriesApi.ts
@@ -4,5 +4,7 @@ import { Country } from './index.dto';
 const BASE_URL = 'https://restcountries.com/v3.1';
 
 export const getAllCountries = () => get<Country[]>(`${BASE_URL}/all`);
-export const getCountryByCode = (code: string) =>
-  get<Country[]>(`${BASE_URL}/alpha/${code}`);
+export const getCountriesByCode = (codes: string[] | string) =>
+  get<Country[]>(
+    `${BASE_URL}/alpha?codes=${typeof codes === 'string' ? codes : codes.join(',')}`
+  );
