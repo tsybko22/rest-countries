ac92be2a38a172a20bb60aef7e92982793621169
Ivan Tsybko <tsybko22@gmail.com>
2024-02-16 15:44:56 +0200

    add CountryInfo component

---
+++ src/components/CountryInfo/CountryInfo.tsx
@@ -0,0 +1,77 @@
+import { Country } from '@/api/index.dto';
+
+import styles from './CountryInfo.module.scss';
+
+interface CountryInfoProps {
+  country: Country;
+}
+
+const CountryInfo = ({ country }: CountryInfoProps) => {
+  const getLanguages = (): string => {
+    const languages: string[] = [];
+
+    for (const value of Object.values(country.languages)) {
+      languages.push(value);
+    }
+
+    return languages.join(',');
+  };
+
+  const getCurrencies = (): string => {
+    const currencies: string[] = [];
+
+    for (const value of Object.values(country.currencies)) {
+      currencies.push(value.name);
+    }
+
+    return currencies.join(', ');
+  };
+
+  return (
+    <article className={styles.country}>
+      <img
+        className={styles.countryImg}
+        src={country.flags.svg}
+        alt={country.flags.alt}
+      />
+      <h1 className={styles.countryTitle}>{country.name.common}</h1>
+      <h2 className={styles.countrySubTitle}>{country.name.official}</h2>
+      <div className={styles.countryWrapper}>
+        <div className={styles.countryBadge}>
+          <span>Population</span>
+          <span>{country.population.toLocaleString()}</span>
+        </div>
+        <div className={styles.countryBadge}>
+          <span>Area</span>
+          <span>{country.area.toLocaleString()}</span>
+        </div>
+      </div>
+      <table className={styles.countryTable}>
+        <tbody>
+          <tr className={styles.countryTableRow}>
+            <th className={styles.countryTableHeading}>Capital</th>
+            <td className={styles.countryTableData}>{country.capital}</td>
+          </tr>
+          <tr className={styles.countryTableRow}>
+            <th className={styles.countryTableHeading}>Subregion</th>
+            <td className={styles.countryTableData}>{country.subregion}</td>
+          </tr>
+          <tr className={styles.countryTableRow}>
+            <th className={styles.countryTableHeading}>Language</th>
+            <td className={styles.countryTableData}>{getLanguages()}</td>
+          </tr>
+          <tr className={styles.countryTableRow}>
+            <th className={styles.countryTableHeading}>Currencies</th>
+            <td className={styles.countryTableData}>{getCurrencies()}</td>
+          </tr>
+          <tr className={styles.countryTableRow}>
+            <th className={styles.countryTableHeading}>Continents</th>
+            <td className={styles.countryTableData}>{country.continents}</td>
+          </tr>
+        </tbody>
+      </table>
+    </article>
+  );
+};
+
+export default CountryInfo;
--- src/pages/CountryPage/CountryPage.tsx
+++ src/pages/CountryPage/CountryPage.tsx
@@ -1,5 +1,44 @@
+import { useFetch } from '@/helpers/hooks/useFetch';
+import { Navigate, useParams } from 'react-router-dom';
+
+import { getCountriesByCode } from '@/api/countriesApi';
+import { type Country } from '@/api/index.dto';
+
+import CountryInfo from '@/components/CountryInfo';
+import MainContainer from '@/components/MainContainer';
+import NeighbouringCountries from '@/components/NeighbouringCountries';
+import Loader from '@/components/ui/Loader';
+
+import styles from './CountryPage.module.scss';
+
 const CountryPage = () => {
-  return <div>CountryDetailsPage</div>;
+  const { countryCode } = useParams();
+  const { data, isLoading, error } = useFetch<Country[]>(
+    getCountriesByCode,
+    countryCode as string
+  );
+
+  if (error) {
+    return (
+      <Navigate
+        to="/error-page"
+        replace={true}
+      />
+    );
+  }
+
+  return (
+    <MainContainer className={styles.countryContainer}>
+      {data && !isLoading ? (
+        <>
+          <CountryInfo country={data[0]} />
+          {data[0].borders && <NeighbouringCountries borders={data[0].borders} />}
+        </>
+      ) : (
+        <Loader className={styles.loader} />
+      )}
+    </MainContainer>
+  );
 };
 
 export default CountryPage;
