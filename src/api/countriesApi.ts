030afd1bdd0a43b7d9a3a9801b72972b6b9287e6
Ivan Tsybko <tsybko22@gmail.com>
2024-02-16 14:45:04 +0200

    fix styles

--- src/api/countriesApi.ts
+++ src/api/countriesApi.ts
@@ -4,7 +4,5 @@ import { Country } from './index.dto';
 const BASE_URL = 'https://restcountries.com/v3.1';
 
 export const getAllCountries = () => get<Country[]>(`${BASE_URL}/all`);
-export const getCountriesByCode = (codes: string[] | string) =>
-  get<Country[]>(
-    `${BASE_URL}/alpha?codes=${typeof codes === 'string' ? codes : codes.join(',')}`
-  );
+export const getCountriesByCode = (codes?: string) =>
+  get<Country[]>(`${BASE_URL}/alpha?codes=${codes}`);
