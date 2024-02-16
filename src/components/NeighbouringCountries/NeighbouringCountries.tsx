0cb6694ec91ba7cf04215768fa15e03787c8fd68
Ivan Tsybko <tsybko22@gmail.com>
2024-02-16 16:38:29 +0200

    change skeleton position

--- src/components/NeighbouringCountries/NeighbouringCountries.tsx
+++ src/components/NeighbouringCountries/NeighbouringCountries.tsx
@@ -19,8 +19,6 @@ const NeighbouringCountries = ({ borders }: NeighbouringCountriesProps) => {
     error,
   } = useFetch<Country[]>(getCountriesByCode, borders.join(','));
 
-  if (isLoading) return <NeighboursSkeleton count={3} />;
-
   if (error) {
     return (
       <Navigate
@@ -33,25 +31,29 @@ const NeighbouringCountries = ({ borders }: NeighbouringCountriesProps) => {
   return (
     <div className={styles.neighboursWrapper}>
       <h3 className={styles.neighboursTitle}>Neighbouring Countries</h3>
-      <ul className={styles.neighbours}>
-        {neighbours &&
-          neighbours.map((neighbour) => (
-            <Link
-              key={neighbour.cca2}
-              to={`/country/${neighbour.cca2}`}
-              replace
-            >
-              <li>
-                <img
-                  className={styles.neighboursImg}
-                  src={neighbour.flags.svg}
-                  alt={neighbour.flags.alt}
-                />
-                <h3 className={styles.neighboursItemTitle}>{neighbour.name.common}</h3>
-              </li>
-            </Link>
-          ))}
-      </ul>
+      {!isLoading ? (
+        <ul className={styles.neighbours}>
+          {neighbours &&
+            neighbours.map((neighbour) => (
+              <Link
+                key={neighbour.cca2}
+                to={`/country/${neighbour.cca2}`}
+                replace
+              >
+                <li>
+                  <img
+                    className={styles.neighboursImg}
+                    src={neighbour.flags.svg}
+                    alt={neighbour.flags.alt}
+                  />
+                  <h3 className={styles.neighboursItemTitle}>{neighbour.name.common}</h3>
+                </li>
+              </Link>
+            ))}
+        </ul>
+      ) : (
+        <NeighboursSkeleton count={3} />
+      )}
     </div>
   );
 };
