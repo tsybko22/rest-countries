fe692feb07270dee25ae6f23ed9e6c614327a523
Ivan Tsybko <tsybko22@gmail.com>
2024-02-17 16:12:12 +0200

    add Checkbox component

---
+++ src/components/ui/Checkbox/Checkbox.tsx
@@ -0,0 +1,25 @@
+import { type InputHTMLAttributes } from 'react';
+
+import styles from './Checkbox.module.scss';
+
+//Exclude "type" prop because for this implementation component always gonna be checkbox type
+interface CheckboxProps extends Omit<InputHTMLAttributes<HTMLInputElement>, 'type'> {
+  labelText?: string;
+}
+
+const Checkbox = ({ labelText, checked, ...props }: CheckboxProps) => {
+  return (
+    <label className={styles.checkboxLabel}>
+      <input
+        className={styles.checkboxInput}
+        type="checkbox"
+        checked={checked}
+        {...props}
+      />
+      <span className={styles.checkbox}></span>
+      {labelText}
+    </label>
+  );
+};
+
+export default Checkbox;
