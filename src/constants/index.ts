b3d4bae7a00497fedc459686bf558984051bd57b
Ivan Tsybko <tsybko22@gmail.com>
2024-02-22 16:03:43 +0200

    add constants file

---
+++ src/constants/index.ts
@@ -0,0 +1,15 @@
+export const SELECT_OPTIONS = [
+  { value: 'population', label: 'Population' },
+  { value: 'name', label: 'Name' },
+  { value: 'area', label: 'Area' },
+];
+
+export const RADIOBUTTONS_LIST = [
+  { name: 'region', value: 'all', defaultChecked: true },
+  { name: 'region', value: 'europe' },
+  { name: 'region', value: 'americas' },
+  { name: 'region', value: 'antarctic' },
+  { name: 'region', value: 'africa' },
+  { name: 'region', value: 'asia' },
+  { name: 'region', value: 'oceania' },
+];
