5742751e52b6e852968efc37fb702a0ee64a8f66
Ivan Tsybko <tsybko22@gmail.com>
2024-02-22 15:06:21 +0200

    fix search width

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -2,12 +2,21 @@ import Input from '../ui/Input';
 
 import styles from './Search.module.scss';
 
-const Search = () => {
+interface SearchProps {
+  searchTerm: string;
+  onSearch: (searchTerm: string) => void;
+}
+
+const Search = ({ searchTerm, onSearch }: SearchProps) => {
   return (
     <div className={styles.search}>
       <Input
         className={styles.searchInput}
-        placeholder="Search by Name, Region, Subregion"
+        placeholder="Search by Name"
+        value={searchTerm}
+        onChange={(evt) => {
+          onSearch(evt.target.value);
+        }}
       />
     </div>
   );
