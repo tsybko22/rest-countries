fecca7a6abbc2c86aa12fe3682084b6d1eebebdf
Ivan Tsybko <tsybko22@gmail.com>
2024-02-23 19:37:32 +0200

    add text when the filtering return no results

--- src/components/CountriesTable/CountriesTable.tsx
+++ src/components/CountriesTable/CountriesTable.tsx
@@ -20,27 +20,38 @@ const CountriesTable = ({ countries }: CountriesTableProps) => {
           <th className={styles.tableHeading}>Region</th>
         </tr>
       </thead>
-
       <tbody>
-        {countries.map((country) => (
-          <tr key={country.cca2}>
-            <td className={styles.tableData}>
-              <img
-                className={styles.tableImg}
-                src={country.flags.svg}
-                alt={country.flags.alt}
-              />
-            </td>
-            <td className={styles.tableData}>
-              <Link to={`/country/${country.cca2}`}>
-                {window.innerWidth >= 576 ? country.name.common : country.cca2}
-              </Link>
-            </td>
-            <td className={styles.tableData}>{country.population.toLocaleString()}</td>
-            <td className={styles.tableData}>{country.area.toLocaleString()}</td>
-            <td className={styles.tableData}>{country.region}</td>
+        {countries.length === 0 ? (
+          <tr>
+            <th colSpan={5}>
+              <h2 className={styles.tableTitle}>No results.</h2>
+            </th>
           </tr>
-        ))}
+        ) : (
+          <>
+            {countries.map((country) => (
+              <tr key={country.cca2}>
+                <td className={styles.tableData}>
+                  <img
+                    className={styles.tableImg}
+                    src={country.flags.svg}
+                    alt={country.flags.alt}
+                  />
+                </td>
+                <td className={styles.tableData}>
+                  <Link to={`/country/${country.cca2}`}>
+                    {window.innerWidth >= 576 ? country.name.common : country.cca2}
+                  </Link>
+                </td>
+                <td className={styles.tableData}>
+                  {country.population.toLocaleString()}
+                </td>
+                <td className={styles.tableData}>{country.area.toLocaleString()}</td>
+                <td className={styles.tableData}>{country.region}</td>
+              </tr>
+            ))}
+          </>
+        )}
       </tbody>
     </table>
   );
